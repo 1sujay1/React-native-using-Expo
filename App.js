1a0926a9e06a12b73b6a5dc3204d1263587924ec
Sujay <70701029+1sujay1@users.noreply.github.com>
2021-12-25 23:30:35 +0530

    React native demo Tracker project using Expo

---
+++ App.js
@@ -0,0 +1,43 @@
+// import 'react-native-gesture-handler';
+import React from 'react';
+import { createAppContainer, createSwitchNavigator } from 'react-navigation';
+import { createStackNavigator } from 'react-navigation-stack';
+import { createBottomTabNavigator } from 'react-navigation-tabs';
+
+import AccountScreen from './src/screens/AccountScreen';
+import SignInScreen from './src/screens/SignInScreen';
+import SignUpScreen from './src/screens/SignUpScreen';
+import TrackCreateScreen from './src/screens/TrackCreateScreen';
+import TrackDetailScreen from './src/screens/TrackDetailScreen';
+import TrackListScreen from './src/screens/TrackListScreen';
+import { Provider as AuthProvider } from './src/context/AuthContext';
+import LoadingScreen from './src/screens/LoadingScreen';
+
+import { setNavigator } from './src/navigationRef';
+
+const switchNavigator = createSwitchNavigator({
+  Load: LoadingScreen,
+  loginFlow: createStackNavigator({
+    SignUp: SignUpScreen,
+    SignIn: SignInScreen
+  }),
+  mainFlow: createBottomTabNavigator({
+    trackListFlow: createStackNavigator({
+      TrackList: TrackListScreen,
+      TrackDetail: TrackDetailScreen
+    }),
+    TrackCreate: TrackCreateScreen,
+    Account: AccountScreen
+  })
+});
+
+
+const App = createAppContainer(switchNavigator);
+
+export default () => {
+  return (
+    <AuthProvider>
+      <App ref={(navigator) => { setNavigator(navigator) }} />
+    </AuthProvider>
+  )
+}
